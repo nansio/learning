888894213af28cd7ded890f3bbb64fe1b0c01c4b
yanan <iyanan@hotmail.com>
2020-08-26 16:27:53 +0800

    初试elementui-tabs

--- elements/src/App.vue
+++ elements/src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-tabs v-model="activeTabName"
+      type="border-card"
+      @tab-click="tabClick"
+      tab-position="left"
+      >
+      <el-tab-pane
+        key=Tabs
+        name=tabs>
+        <span slot="label">
+          <i class="el-icon-date"></i>
+          我的行程
+        </span>
+        my journey
+      </el-tab-pane>
+    </el-tabs>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      items: [
+        {
+          key: 'Tabs', label: 'Tabs标签页', name: 'tabs'
+        },
+        {
+          key: 'ToolTip', label: 'ToolTip文字提示', name: 'tooltips'
+        },
+        {
+          key: 'NavMenu', label: 'NavMenu导航菜单', name: 'navmenu'
+        },
+      ],
+      activeTabName: 'tabs'
+    }
+  },
   components: {
-    HelloWorld
+    
+  },
+  methods: {
+    tabClick(tab){
+      console.log(tab.name);
+    }
   }
 }
 </script>
@@ -23,6 +57,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
