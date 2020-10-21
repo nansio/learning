ced96483c041d3a95dca5ed68fba2011a24f47f4
yanan <iyanan@hotmail.com>
2020-10-21 11:18:39 +0800

    Math常用的函数

---
+++ js/math.js
@@ -0,0 +1,33 @@
+
+console.log(Math.ceil(1235.69));
+
+console.log(Math.floor(1235.659));
+
+console.log(Math.max(1,3,45,6677,74,2));
+console.log(Math.min(1,3,45,6677,74,2));
+
+console.log(Math.pow(2, 365))
+
+console.log('Random Number 1 : ', Math.random());
+console.log('Random Number 2 : ', Math.random());
+
+console.log((Math.round(1.34)));
+console.log((Math.round(1.54)));
+
+console.log(Math.abs(Number.MIN_VALUE));
+console.log(Math.abs(-100));
+
+console.log(Math.sin(Math.PI/3));
+console.log(Math.asin(0.5));
+console.log(Math.cos(Math.PI/3));
+console.log(Math.acos(0.5));
+console.log(Math.tan(Math.PI/3));
+console.log(Math.atan(0.3));
+
+console.log(Math.exp(1));
+
+console.log(Math.log(Math.E))
+
+console.log(Math.sqrt(100))
+
+console.log(Math.valueOf({value: 123}));
