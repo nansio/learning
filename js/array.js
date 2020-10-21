5c301d0fb045b04f4d0cb230a8db86e82bcadd10
yanan <iyanan@hotmail.com>
2020-10-21 10:31:42 +0800

    Array常用函数

--- js/array.js
+++ js/array.js
@@ -111,25 +111,3 @@ console.log(Array.isArray(shun))
 console.log(shun.copyWithin(2,0,2))
 
 console.log(shun.find(item => item > 105))
-
-const hello = 'hel1lo theree2'
-
-console.log('3st char is : ' + hello.charAt((3)))
-console.log('3st charCode is : ' + hello.charCodeAt((3)))
-
-console.log(hello.match('there'))
-console.log(hello.match(/\d+/g))
-
-const greeting = '  hello, man   '
-console.log(greeting.replace('man','girl'))
-console.log(greeting.search('ell'))
-
-console.log(greeting.split('',3))
-
-console.log(greeting.toLocaleLowerCase())
-console.log(greeting.toLowerCase ())
-console.log(greeting.toLocaleUpperCase ())
-console.log(greeting.toUpperCase())
-
-console.log(greeting.substr(0,1))
-console.log(greeting.trim())
