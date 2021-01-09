59a2493642e81effb2a85ec39c26e3216e8f7667
yanan <iyanan@hotmail.com>
2021-01-09 11:08:19 +0800

    Vue组件Props作样式

---
+++ vue/props4style/app.vue
@@ -0,0 +1,28 @@
+<!--
+ * @description: 
+ * @version: 
+ * @Author: ya`nan
+ * @Date: 2021-01-09 10:51:40
+ * @LastEditors: ya`nan
+ * @LastEditTime: 2021-01-09 11:02:06
+-->
+<template>
+  <div>
+    <box :height="500" :width="500" bgcolor="#48a047" />
+  </div>
+</template>
+
+<script>
+
+import Box from './box'
+
+export default {
+  components: { 
+    Box
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/props4style/box.vue
@@ -0,0 +1,52 @@
+<!--
+ * @description: 
+ * @version: 
+ * @Author: ya`nan
+ * @Date: 2021-01-09 10:51:51
+ * @LastEditors: ya`nan
+ * @LastEditTime: 2021-01-09 11:01:17
+-->
+<template>
+  <div class="box" :style="styleVar">
+    the style of this div comes from props
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    height: {
+      type: Number,
+      default: 300
+    },
+    width: {
+      type: Number,
+      default: 300
+    },
+    bgcolor: {
+      type: String,
+      default: '#f01ab3'
+    }
+  },
+  computed: {
+    styleVar() {
+      return {
+        '--box-height': this.height + 'px',
+        '--box-width' : this.width + 'px',
+        '--box-bgcolor': this.bgcolor
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.box{
+  height: var(--box-height);
+  width: var(--box-width);
+  background: var(--box-bgcolor);
+  text-align: center;
+  line-height: var(--box-height);
+}
+</style>
